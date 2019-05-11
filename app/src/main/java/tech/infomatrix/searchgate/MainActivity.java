712d724fe8a51da83650ad96ebd953a0b9f148c9
ErikOwsiak <erik@owsiak.name>
2019-05-11 10:12:34 +0200

    start andy app

---
+++ app/src/main/java/tech/infomatrix/searchgate/MainActivity.java
@@ -0,0 +1,13 @@
+package tech.infomatrix.searchgate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
